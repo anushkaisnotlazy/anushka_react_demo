d5ace744562dffbbd475827af9d35b381338b1d5
anushkaisnotlazy <anushka.saxena.workspace@gmail.com>
2025-02-24 23:17:32 +0530

    Add Nav bar and so many other things

---
+++ src/PropsDemo.jsx
@@ -0,0 +1,27 @@
+const PropsDemo=()=>{
+    return(
+        <div>
+            <h1>Props Values: </h1>
+
+
+        </div>
+
+
+    );
+
+}
+export default PropsDemo;
+
+// function PropsDemo(){
+//     return(
+//         <div>
+//             <h1>Props Values: </h1>
+
+
+//         </div>
+
+
+//     );
+
+// }
+// export default PropsDemo;
